6d6d4f092c30fe87350aef4ab24fed6e3e6654d7
Cédric Bouysset <bouysset.cedric@gmail.com>
2021-10-07 23:07:43 +0200

    add index and header to smiles selection export

--- mols2grid/templates/js/molstorage.js
+++ mols2grid/templates/js/molstorage.js
@@ -2,16 +2,16 @@ class MolStorage extends Map {
     to_dict() {
         var content = "{";
         for (let [key, value] of this) {
-        content += key + ":" + JSON.stringify(value) + ",";
+            content += key + ":" + JSON.stringify(value) + ",";
         }
         content = content.length > 1 ? content.slice(0, -1) : content;
         content += "}";
         return content
     }
     download_smi(fileName) {
-        var content = "";
+        var content = "SMILES index\n";
         for (let [key, value] of this) {
-            content += value + "\n";
+            content += value + " " + key + "\n";
         }
         var a = document.createElement("a");
         var file = new Blob([content], {type: "text/plain"});
