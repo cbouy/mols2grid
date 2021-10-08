70323b033d8407576ecee62ec0a684e95e358d7d
Cédric Bouysset <bouysset.cedric@gmail.com>
2021-10-08 17:02:12 +0200

    fix filtering for streamlit

--- mols2grid/templates/js/filter.js
+++ mols2grid/templates/js/filter.js
@@ -1,7 +1,10 @@
 let name = {{ grid_id | tojson }};
-if ((typeof mols2grid_lists !== "undefined") && (name in mols2grid_lists)) {
+if (typeof mols2grid_lists !== "undefined") {
     var listObj = mols2grid_lists[name];
-    var kl = listObj.items.length;
+} else if (typeof window.parent.mols2grid_lists !== "undefined") {
+    var listObj = window.parent.mols2grid_lists[name];
+}
+if (typeof listObj !== "undefined") {
     var ids = {{ ids }};
     listObj.filter(function (item) {
         return ids.includes(item.values()["mols2grid-id"]);
